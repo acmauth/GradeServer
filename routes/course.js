5a7b2dcd6cbf3f87f3b421f7bb5e1685797744df
Christina <chriskreza@csd.auth.gr>
2020-02-24 12:48:52 +0200

    Added versions endpoint

--- routes/course.js
+++ routes/course.js
@@ -5,10 +5,11 @@ const CourseController = require('../controllers/CourseController');
 const checkAuth = require('../middleware/check-auth');
 
 /* Course Information */
+router.get('/check_version/:version_id', checkAuth, CourseController.check_version);
 
 router.get('/info/:course_id', checkAuth, CourseController.info);
 
-router.get('/predict/', checkAuth, CourseController.predict);
+router.get('/predict', checkAuth, CourseController.predict);
 
 router.get('/suggest', checkAuth, CourseController.suggest);
 
