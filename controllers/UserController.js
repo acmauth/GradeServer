c731385c81971120020bd9c71b9a413424a3287d
chriskrez <chriskreza@csd.auth.gr>
2020-05-07 11:22:51 +0300

    Change user favorites to update via POST (#16)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,123 +12,32 @@ const UserModel = require("../models/UserModel");
 const CourseModel = require("../models/CourseModel");
 const TeacherModel = require("../models/TeacherModel");
 
-const Favorites = {
-  COURSES: {
-    model: CourseModel,
-    collection: "courses",
-    set: "favorite_subjects"
-  },
-  TEACHERS: {
-    model: TeacherModel,
-    collection: "teachers",
-    set: "favorite_teachers"
-  }
-};
-
-function addFavorite(req, res, favorite) {
-  var list = [];
-  var promises = [];
-  if (!req.body[favorite.collection]) {
-    res.status(400).send();
-    return;
-  }
-  req.body[favorite.collection].forEach(async item => {
-    const query = {};
-    query["_id"] = item;
-    promises.push(
-      favorite.model
-        .countDocuments(query)
-        .exec()
-        .then(count => {
-          if (count > 0) {
-            list.push(item);
-          }
-        })
-        .catch()
-    );
-  });
-
-  Promise.all(promises).then(() => {
-    if (list.length != 0) {
-      const query = {};
-      query[favorite.set] = list;
-      UserModel.updateOne({ _id: req.userData.userId }, { $addToSet: query })
-        .exec()
-        .then(res.status(204).send())
-        .catch();
-    } else {
-      res.status(400).json({
-        message: "Invalid id"
-      });
-    }
-  });
-}
-
-function removeFavorite(req, res, favorite) {
-  var list = [];
-  var promises = [];
-  if (!req.body[favorite.collection]) {
-    res.status(400).send();
-    return;
-  }
-  req.body[favorite.collection].forEach(async item => {
-    const query = {};
-    query[favorite.path] = item;
-    promises.push(
-      favorite.model
-        .countDocuments(query)
-        .exec()
-        .then(count => {
-          if (count > 0) {
-            list.push(item);
-          }
-        })
-        .catch()
-    );
-  });
-
-  Promise.all(promises).then(() => {
-    if (list.length != 0) {
-      const query = {};
-      query[favorite.set] = { $in: list };
-      UserModel.updateOne({ _id: req.userData.userId }, { $pull: query })
-        .exec()
-        .then(res.status(204).send())
-        .catch();
-    } else {
-      res.status(400).json(() => {
-        message: "Invalid id";
-      });
-    }
-  });
-}
-
 function generateToken(user) {
   const token = jwt.sign(
     {
       email: user.email,
-      userId: user._id
+      userId: user._id,
     },
     process.env.tokenSecretKey,
     {
-      expiresIn: process.env.tokenLife
+      expiresIn: process.env.tokenLife,
     }
   );
 
   const refreshToken = jwt.sign(
     {
       email: user.email,
-      userId: user._id
+      userId: user._id,
     },
     process.env.refreshTokenSecret,
     {
-      expiresIn: process.env.refreshTokenLife
+      expiresIn: process.env.refreshTokenLife,
     }
   );
 
   const response = {
     token: token,
-    refreshToken: refreshToken
+    refreshToken: refreshToken,
   };
   tokenList[refreshToken] = response;
   return response;
@@ -137,13 +46,13 @@ function generateToken(user) {
 function updateGrades(user, course) {
   // TODO id exists, grade/year/semester limits
 
-  var passedCourse = user.grades.find(element => element.id === course.id);
+  var passedCourse = user.grades.find((element) => element.id === course.id);
   if (!passedCourse) {
     passedCourse = {
       _id: course.id,
       grade: course.grade,
       year_passed: course.year,
-      semester: course.semester
+      semester: course.semester,
     };
     user.grades.push(passedCourse);
   } else {
@@ -154,21 +63,13 @@ function updateGrades(user, course) {
 }
 
 module.exports = {
-  addFavoriteCourse: (req, res) => {
-    addFavorite(req, res, Favorites.COURSES);
-  },
-
-  addFavoriteTeacher: (req, res) => {
-    addFavorite(req, res, Favorites.TEACHERS);
-  },
-
   changePassword: (req, res) => {
     const previousPassword = req.body.previousPassword;
     const newPassword = req.body.newPassword;
 
     UserModel.findOne({ _id: req.userData.userId })
       .exec()
-      .then(user => {
+      .then((user) => {
         if (!user) {
           return res.status(400).send();
         }
@@ -189,15 +90,15 @@ module.exports = {
             });
           } else {
             res.status(401).json({
-              error: "Invalid password"
+              error: "Invalid password",
             });
           }
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         res.status(500).json({
-          error: "Invalid credentials"
+          error: "Invalid credentials",
         });
       });
   },
@@ -209,17 +110,17 @@ module.exports = {
   getProfile: (req, res) => {
     UserModel.findOne({ _id: req.userData.userId })
       .exec()
-      .then(user => {
+      .then((user) => {
         user = user.toJSON();
         user._id = undefined;
         user.password = undefined;
         user.__v = undefined;
         var promises = [];
-        user.grades.forEach(course => {
+        user.grades.forEach((course) => {
           promises.push(
             CourseModel.findOne({ _id: course._id })
               .exec()
-              .then(passedCourse => {
+              .then((passedCourse) => {
                 if (!passedCourse) {
                   // res.status(400).send();
                   return;
@@ -227,7 +128,7 @@ module.exports = {
 
                 course.name = passedCourse.basic_info.name;
                 course.teacher = [
-                  ...new Set(passedCourse.basic_info.class.teachers)
+                  ...new Set(passedCourse.basic_info.class.teachers),
                 ].join(", ");
               })
               .catch()
@@ -235,11 +136,11 @@ module.exports = {
         });
 
         var subjects = [];
-        user.favorite_subjects.forEach(subject => {
+        user.favorite_subjects.forEach((subject) => {
           promises.push(
             CourseModel.findOne({ _id: subject })
               .exec()
-              .then(course => {
+              .then((course) => {
                 if (!course) {
                   // res.status(400).send();
                   return;
@@ -252,11 +153,11 @@ module.exports = {
         });
 
         var teachers = [];
-        user.favorite_teachers.forEach(teacher => {
+        user.favorite_teachers.forEach((teacher) => {
           promises.push(
             TeacherModel.findOne({ _id: teacher })
               .exec()
-              .then(selected => {
+              .then((selected) => {
                 if (!selected) {
                   // res.status(400).send();
                   return;
@@ -280,10 +181,10 @@ module.exports = {
   login: (req, res) => {
     UserModel.findOne({ email: req.body.email })
       .exec()
-      .then(user => {
+      .then((user) => {
         if (!user) {
           return res.status(401).json({
-            error: "Invalid credentials"
+            error: "Invalid credentials",
           });
         }
         bcrypt.compare(req.body.password, user.password, (err, success) => {
@@ -296,14 +197,14 @@ module.exports = {
             return res.status(200).json(generateToken(user));
           }
           return res.status(401).json({
-            error: "Invalid credentials"
+            error: "Invalid credentials",
           });
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         res.status(500).json({
-          error: err
+          error: err,
         });
       });
   },
@@ -312,7 +213,7 @@ module.exports = {
     const id = req.userData.userId;
     const fields = {};
 
-    PostRegistrationSchema.eachPath(path => {
+    PostRegistrationSchema.eachPath((path) => {
       if (req.body[path]) {
         fields[`postRegistrationInfo.${path}`] = req.body[path];
       }
@@ -321,26 +222,18 @@ module.exports = {
     UserModel.updateOne(
       { _id: id },
       {
-        $set: fields
+        $set: fields,
       }
     )
       .exec()
-      .then(status => {
+      .then((status) => {
         if (status.nModified == 1) {
           res.status(200).send(status);
         } else {
           res.status(400).send(status);
         }
       })
-      .catch(err => res.status(400).send(err));
-  },
-
-  removeFavoriteCourse: (req, res) => {
-    removeFavorite(req, res, Favorites.COURSES);
-  },
-
-  removeFavoriteTeacher: (req, res) => {
-    removeFavorite(req, res, Favorites.TEACHERS);
+      .catch((err) => res.status(400).send(err));
   },
 
   refreshToken: (req, res) => {
@@ -349,16 +242,16 @@ module.exports = {
       const token = jwt.sign(
         {
           email: req.body.email,
-          userId: req.body._id
+          userId: req.body._id,
         },
         process.env.refreshTokenSecret,
         {
-          expiresIn: process.env.tokenLife
+          expiresIn: process.env.tokenLife,
         }
       );
 
       const response = {
-        token: token
+        token: token,
       };
 
       tokenList[req.body.refreshToken].token = token;
@@ -371,32 +264,32 @@ module.exports = {
   signup: (req, res) => {
     UserModel.find({ email: req.body.email })
       .exec()
-      .then(user => {
+      .then((user) => {
         if (user.length > 0) {
           return res.status(409).json({
-            message: "Mail exists"
+            message: "Mail exists",
           });
         } else {
           bcrypt.hash(req.body.password, 10, (err, hash) => {
             if (err) {
               return res.status(400).json({
-                error: "Invalid request"
+                error: "Invalid request",
               });
             } else {
               const user = new UserModel({
                 _id: new mongoose.Types.ObjectId(),
                 email: req.body.email,
-                password: hash
+                password: hash,
               });
               user
                 .save()
                 .then(() => {
                   res.status(201).json(generateToken(user));
                 })
-                .catch(err => {
+                .catch((err) => {
                   console.log(err);
                   res.status(500).json({
-                    error: "Invalid credentials"
+                    error: "Invalid credentials",
                   });
                 });
             }
@@ -409,7 +302,7 @@ module.exports = {
     const id = req.userData.userId;
     const fields = {};
 
-    BioSchema.eachPath(path => {
+    BioSchema.eachPath((path) => {
       if (req.body[path]) {
         fields[`bio.${path}`] = req.body[path];
       }
@@ -419,11 +312,11 @@ module.exports = {
     UserModel.updateOne(
       { _id: id },
       {
-        $set: fields
+        $set: fields,
       }
     )
       .exec()
-      .then(status => {
+      .then((status) => {
         if (status.nModified == 1) {
           // UserModel.findById(id)
           //   .exec()
@@ -438,18 +331,77 @@ module.exports = {
           res.status(400).send(status);
         }
       })
-      .catch(err => res.status(400).send(err));
+      .catch((err) => res.status(400).send(err));
+  },
+
+  updateFavorites: (req, res) => {
+    var validTeachers = [];
+    var validCourses = [];
+    var promises = [];
+
+    if (req.body.teachers) {
+      promises.push(
+        TeacherModel.find({ _id: { $in: req.body.teachers } })
+          .select("_id")
+          .then((teachers) => {
+            if (teachers) {
+              validTeachers = teachers.map((t) => t._id);
+            }
+          })
+          .catch((err) => {
+            console.error(`Error during teacher find():\n${err}`);
+            return res.status(500).send();
+          })
+      );
+    }
+
+    if (req.body.courses) {
+      promises.push(
+        CourseModel.find({ _id: { $in: req.body.courses } })
+          .select("_id")
+          .then((courses) => {
+            if (courses) {
+              validCourses = courses.map((c) => c._id);
+            }
+          })
+          .catch((err) => {
+            console.error(`Error during course find():\n${err}`);
+            return res.status(500).send();
+          })
+      );
+    }
+
+    Promise.all(promises).then(() => {
+      var set = {};
+
+      if (validCourses.length) {
+        set.favorite_subjects = validCourses;
+      }
+
+      if (validTeachers.length) {
+        set.favorite_teachers = validTeachers;
+      }
+
+      UserModel.updateOne({ _id: req.userData.userId }, { $set: set })
+        .then(() => {
+          return res.status(204).send();
+        })
+        .catch((err) => {
+          console.error(`Error during user update():\n${err}`);
+          return res.status(500).send();
+        });
+    });
   },
 
   updateGradesList: (req, res) => {
     UserModel.findOne({ _id: req.userData.userId })
       .exec()
-      .then(user => {
+      .then((user) => {
         if (!user) {
           res.status(400).send();
           return;
         }
-        req.body.courses.forEach(course => {
+        req.body.courses.forEach((course) => {
           updateGrades(user, course);
         });
         user.save();
@@ -465,7 +417,7 @@ module.exports = {
     var dt = new Date().getTime();
     var file = dt + "." + ext;
     var filePath = `./files/${file}`;
-    req.files.grades.mv(filePath, err => {
+    req.files.grades.mv(filePath, (err) => {
       if (err) {
         res.status(500).send();
         console.log(err);
@@ -477,7 +429,7 @@ module.exports = {
         (error, stdout, stderr) => {
           if (error) {
             res.status(500).json({
-              error: "Invalid file"
+              error: "Invalid file",
             });
             console.log(error);
             return;
@@ -486,24 +438,24 @@ module.exports = {
           var gradesJSON = require(`.${jsonPath}`);
           UserModel.findOne({ _id: req.userData.userId })
             .exec()
-            .then(user => {
+            .then((user) => {
               if (!user) {
                 res.status(400).send();
                 return;
               }
               var promises = [];
-              gradesJSON.courses.forEach(course => {
+              gradesJSON.courses.forEach((course) => {
                 if (course.grade) {
                   promises.push(
                     CourseModel.findOne({ "basic_info.code": course.code })
                       .exec()
-                      .then(courseData => {
+                      .then((courseData) => {
                         if (courseData) {
                           var updatedCourse = {
                             id: courseData._id,
                             year: course.year,
                             semester: courseData.basic_info.period,
-                            grade: course.grade
+                            grade: course.grade,
                           };
 
                           updateGrades(user, updatedCourse);
@@ -519,12 +471,12 @@ module.exports = {
             })
             .catch();
           res.status(201).send();
-          fs.unlink(filePath, err => {
+          fs.unlink(filePath, (err) => {
             if (err) {
               console.log(err);
             }
           });
-          fs.unlink(jsonPath, err => {
+          fs.unlink(jsonPath, (err) => {
             if (err) {
               console.log(err);
             }
@@ -532,5 +484,5 @@ module.exports = {
         }
       );
     });
-  }
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,46 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const UserController = require('../controllers/UserController');
-const checkAuth = require('../middleware/check-auth');
-const deleteUser = require('../middleware/delete-user');
+const UserController = require("../controllers/UserController");
+const checkAuth = require("../middleware/check-auth");
+const deleteUser = require("../middleware/delete-user");
 
 /* User profile */
 
-router.get('/download_data', checkAuth, UserController.getData);
+router.get("/download_data", checkAuth, UserController.getData);
 
-router.get('/profile', checkAuth, UserController.getProfile);
+router.get("/profile", checkAuth, UserController.getProfile);
 
-router.patch('/profile', checkAuth, UserController.updateBio);
+router.patch("/profile", checkAuth, UserController.updateBio);
 
-router.patch('/change_password', checkAuth, UserController.changePassword)
+router.patch("/change_password", checkAuth, UserController.changePassword);
 
-router.patch('/grades/list', checkAuth, UserController.updateGradesList);
+router.patch("/grades/list", checkAuth, UserController.updateGradesList);
 
-router.put('/grades/pdf', checkAuth, UserController.updateGradesPDF);
+router.put("/grades/pdf", checkAuth, UserController.updateGradesPDF);
 
-router.delete('/request_deletion', checkAuth, deleteUser);
+router.delete("/request_deletion", checkAuth, deleteUser);
 
 /* Favorites */
 
-router.patch('/favorites/course', checkAuth, UserController.addFavoriteCourse);
-
-router.delete(
-  '/favorites/course',
-  checkAuth,
-  UserController.removeFavoriteCourse
-);
-
-router.patch(
-  '/favorites/teacher',
-  checkAuth,
-  UserController.addFavoriteTeacher
-);
-
-router.delete(
-  '/favorites/teacher',
-  checkAuth,
-  UserController.removeFavoriteTeacher
-);
+router.post("/favorites", checkAuth, UserController.updateFavorites);
 
 module.exports = router;
